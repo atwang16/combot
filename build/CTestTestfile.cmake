3dd3fd7ca90aaf0c45f076dddc83050221e4f8dc
Austin Wang <austin.t.wang@gmail.com>
2016-11-27 19:56:38 -0800

    Moved contents from catkin_ws to general folder

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/austin/combot/src
+# Build directory: /home/austin/combot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(teleop)
